58b9e0976438aa47c2fdef9007330f502048d34c
htomar6397 <htomar6397@gmail.com>
2026-02-10 00:31:52 +0530

    build

---
+++ public/build/assets/trending-down-BVdc8l8P.js
@@ -0,0 +1,1 @@
+import{c as n}from"./AdminLayout-2TTQmOgj.js";const o=[["path",{d:"M5 12h14",key:"1ays0h"}]],c=n("minus",o);const t=[["path",{d:"M16 17h6v-6",key:"t6n2it"}],["path",{d:"m22 17-8.5-8.5-5 5L2 7",key:"x473p"}]],s=n("trending-down",t);export{c as M,s as T};
